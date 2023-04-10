997ea57ace9f040790e987e97aa60c4fd01608ff
AbdimalikHassan <abdihassan377@gmail.com>
2023-04-09 22:00:57 -0400

    Added Database and hardcoded creation of database entries

--- Code/app/src/main/java/com/example/courselistplus/NavigationActivity.java
+++ Code/app/src/main/java/com/example/courselistplus/NavigationActivity.java
@@ -3,6 +3,7 @@ package com.example.courselistplus;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.courselistplus.databinding.ActivityNavigationBinding;
 
+import java.util.ArrayList;
+
 public class NavigationActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -24,6 +27,51 @@ public class NavigationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // BEGIN DATABASE INITIALIZATION
+        // Hard-coded courses to prepopulate the database on every run
+        CourseModel emergingDiseases = new CourseModel(-1, 21621,
+                "CHEM 150 01", "C150", "Emerging Diseases",
+                "Sher, Beverly", 4, "MWF", "1200-1250",
+                17, 16, "Open");
+        CourseModel entrepreneurshipInCS = new CourseModel(-1, 24354,
+                "CSCI 425 01", "C400, IN", "Entrepreneurship in CS",
+                "Kemper, Peter", 3, "TR", "1100-1220",
+                45, 44, "Open");
+        CourseModel dataMining = new CourseModel(-1, 27294,
+                "CSCI 436 01", "None", "Data Mining",
+                "Shao, Huajie", 3, "MW", "1400-1520",
+                40, 27, "Open");
+        CourseModel operatingSystems = new CourseModel(-1, 25722,
+                "CSCI 444 01", "None", "Operating Systems",
+                "Kumar, Pradeep", 3, "TR", "1230-1350",
+                40, 35, "Open");
+        CourseModel computerAndNetworkSecurity = new CourseModel(-1, 24957,
+                "CSCI 454 01", "None", "Computer and Network Security",
+                "Evtyushkin, Dmitry", 3, "TR", "1400-1520",
+                40, 34, "Open");
+
+        DataAccessObject dataAccessObject = new DataAccessObject(this);
+        ArrayList<CourseModel> courseModelList = new ArrayList<CourseModel>(){{
+            add(emergingDiseases);
+            add(entrepreneurshipInCS);
+            add(dataMining);
+            add(operatingSystems);
+            add(computerAndNetworkSecurity);
+        }};
+
+        for(int i = 0; i < 5; i++){
+            boolean success = dataAccessObject.addOne(courseModelList.get(i));
+            Toast.makeText(this, "Inserting " +
+                    courseModelList.get(i).getCourseTitle() + " Success = " + success,
+                    Toast.LENGTH_SHORT).show();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        // END DATABASE INITIALIZATION
 
         binding = ActivityNavigationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
