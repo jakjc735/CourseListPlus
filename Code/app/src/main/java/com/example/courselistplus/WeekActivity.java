88a59a1b9f87612bf77e1f1fd5d69b9c0caf3f64
Amir Hassan Shariatmadari <ahshariatmadar@email.wm.edu>
2023-04-21 23:51:54 -0400

    fixed merge conflicts

---
+++ Code/app/src/main/java/com/example/courselistplus/WeekActivity.java
@@ -0,0 +1,82 @@
+package com.example.courselistplus;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.courselistplus.R;
+import com.example.courselistplus.ui.Calendar.CalendarAdapter;
+import com.example.courselistplus.ui.Calendar.CalendarUtils;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static com.example.courselistplus.ui.Calendar.CalendarUtils.daysInMonthArray;
+import static com.example.courselistplus.ui.Calendar.CalendarUtils.daysInWeekArray;
+import static com.example.courselistplus.ui.Calendar.CalendarUtils.monthYearFromDate;
+
+/**
+ * Sets up the week view after you click on the weekly button
+ * Also allows you to click on each day in the weekly view
+ *
+ * @author JC Alvarez
+ */
+
+public class WeekActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week);
+        initWidgets();
+        setWeekView();
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearDisplay);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setWeekView() {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void previousWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void nextWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+        CalendarUtils.selectedDate = date;
+        setWeekView();
+    }
+}
