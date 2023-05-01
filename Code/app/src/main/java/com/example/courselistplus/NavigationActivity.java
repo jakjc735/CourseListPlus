e732651adb582f6180b19263bf919aed33fd00f0
amui <amui@email.wm.edu>
2023-04-30 20:47:45 -0400

    Home page updates before pulling from main

--- Code/app/src/main/java/com/example/courselistplus/NavigationActivity.java
+++ Code/app/src/main/java/com/example/courselistplus/NavigationActivity.java
@@ -41,7 +41,7 @@ public class NavigationActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_profile)
+                R.id.nav_homepage, R.id.nav_gallery, R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(
